407b0203a93d498d21b53106bfb6e63e6afbe405
JordanAceto <jordanaceto@gmail.com>
2019-12-21 16:36:11 -0500

    refactor complex hw.c

--- complex_blinking_hws/hw.c
+++ complex_blinking_hws/hw.c
@@ -7,7 +7,25 @@
 
 static volatile uint32_t *gpio;
 
+const uint8_t LED_PIN = 17; // gpio17
+
 void setup();
+
+typedef enum
+{
+    INPUT,
+    OUTPUT
+} pin_mode_t;
+
+typedef enum
+{
+    LOW,
+    HIGH
+} digital_write_t;
+
+void pinMode(const uint8_t gpio_pin_num, const pin_mode_t pin_mode);
+
+void digitalWrite(const uint8_t gpio_pin_num, const digital_write_t value);
 
 int main()
 {
@@ -28,7 +46,6 @@ int main()
 
 void setup()
 {
-
     int fd;
 
     if ((fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0)
@@ -37,9 +54,16 @@ void setup()
         exit(-1);
     }
 
-    const uint32_t OFFSET = 0x3F200000;
-
-    gpio = (uint32_t *)mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, OFFSET);
+    // start address for BCM2837 gpio memory region
+    const uint32_t GPFSEL0 = 0x3F200000;
+    
+    gpio = (uint32_t *)mmap
+        (0, 
+        getpagesize(), 
+        PROT_READ|PROT_WRITE, 
+        MAP_SHARED, 
+        fd, 
+        GPFSEL0);
 
     if (gpio == MAP_FAILED)
     {
@@ -47,6 +71,36 @@ void setup()
         exit(-1);
     }
 
-    // set gpio17 as output
-    *(gpio + 1) = (*(gpio + 1) & ~(7 << 21)) | (1 << 21);
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void pinMode(const uint8_t gpio_pin_num, const pin_mode_t pin_mode)
+{
+    // see datasheet pg 91
+    const uint32_t NUM_PINS_PER_GPFSEL_REG = 10;
+
+    const uint32_t GPFSEL_OFFSET = gpio_pin_num / NUM_PINS_PER_GPFSEL_REG;
+    
+    // each pin gets three bits in its GPFSEL register which set its mode
+    const uint32_t PIN_POSITION = (gpio_pin_num % 10) * 3;
+
+    // clear the 3 bits that set the pin mode in GPFSELn
+    *(gpio + GPFSEL_OFFSET) &= ~(0b111 << PIN_POSITION);
+
+    if (pin_mode == OUTPUT)
+    {
+        // set the lsb in GPFSELn to make the pin an output
+        *(gpio + GPFSEL_OFFSET) |= (1 << PIN_POSITION);
+    }
+}
+
+void digitalWrite(const uint8_t gpio_pin_num, const digital_write_t value)
+{
+    // see datasheet pg 90
+    const uint32_t GPSET0_OFFSET = 7;
+    const uint32_t GPCLR0_OFFSET = 10;
+
+    const uint32_t OFFSET = (value == LOW) ? GPCLR0_OFFSET : GPSET0_OFFSET;
+    
+    *(gpio + OFFSET) = 1 << gpio_pin_num;
 }
