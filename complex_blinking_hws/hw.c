f016ef79d8fa7bd30fbb2e68d109c6c953940aa9
JordanAceto <jordanaceto@gmail.com>
2019-12-22 07:37:06 -0500

    add checks for invalid gpio pins

--- complex_blinking_hws/hw.c
+++ complex_blinking_hws/hw.c
@@ -1,15 +1,21 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 // pointer to BCM2837 memory map
 static volatile uint32_t *gpio;
 
-// warning, there is currently no error checking, 
-// so make sure you pass in a valid pin number!
+// valid GPIO pins fall in this range, inclusive
+const uint8_t LOWEST_GPIO_PIN = 2;
+const uint8_t HIGHEST_GPIO_PIN = 27;
+
+bool isValidPin(const uint8_t pin_num);
+
 const uint8_t LED_PIN = 17; // gpio17
 
 void setup();
@@ -76,43 +82,52 @@ void setup()
     pinMode(LED_PIN, OUTPUT);
 }
 
-/* 
-note: there should really be some error checking to make sure you're
-actually passing in a valid pin, there is the possibility of messing
-up your pi if you pass garbage in to this function, beware!
-*/
 void pinMode(const uint8_t gpio_pin_num, const pin_mode_t pin_mode)
 {
-    // see datasheet pg 91
-    const uint32_t NUM_PINS_PER_GPFSEL_REG = 10;
-
-    const uint32_t GPFSEL_OFFSET = gpio_pin_num / NUM_PINS_PER_GPFSEL_REG;
-    
-    // each pin gets three bits in its GPFSEL register which set its mode
-    const uint32_t PIN_POSITION = (gpio_pin_num % 10) * 3;
-
-    // clear the 3 bits that set the pin mode in GPFSELn
-    *(gpio + GPFSEL_OFFSET) &= ~(0b111 << PIN_POSITION);
-
-    if (pin_mode == OUTPUT)
+    if (isValidPin(gpio_pin_num))
     {
-        // set the lsb in GPFSELn to make the pin an output
-        *(gpio + GPFSEL_OFFSET) |= (1 << PIN_POSITION);
+        // see datasheet pg 91
+        const uint32_t NUM_PINS_PER_GPFSEL_REG = 10;
+
+        const uint32_t GPFSEL_OFFSET = gpio_pin_num / NUM_PINS_PER_GPFSEL_REG;
+        
+        // each pin gets three bits in its GPFSEL register which set its mode
+        const uint32_t PIN_POSITION = (gpio_pin_num % 10) * 3;
+
+        // clear the 3 bits that set the pin mode in GPFSELn
+        *(gpio + GPFSEL_OFFSET) &= ~(0b111 << PIN_POSITION);
+
+        if (pin_mode == OUTPUT)
+        {
+            // set the lsb in GPFSELn to make the pin an output
+            *(gpio + GPFSEL_OFFSET) |= (1 << PIN_POSITION);
+        }
+    }
+    else
+    {
+        // invalid pin, do nothing
     }
 }
 
-/* 
-note: there should really be some error checking to make sure you're
-actually passing in a valid pin, there is the possibility of messing
-up your pi if you pass garbage in to this function, beware!
-*/
 void digitalWrite(const uint8_t gpio_pin_num, const digital_write_t value)
 {
-    // see datasheet pg 90
-    const uint32_t GPSET0_OFFSET = 7;
-    const uint32_t GPCLR0_OFFSET = 10;
+    if (isValidPin(gpio_pin_num)) 
+    {
+        // see datasheet pg 90
+        const uint32_t GPSET0_OFFSET = 7;
+        const uint32_t GPCLR0_OFFSET = 10;
 
-    const uint32_t OFFSET = (value == LOW) ? GPCLR0_OFFSET : GPSET0_OFFSET;
+        const uint32_t OFFSET = (value == LOW) ? GPCLR0_OFFSET : GPSET0_OFFSET;
     
-    *(gpio + OFFSET) = 1 << gpio_pin_num;
+        *(gpio + OFFSET) = 1 << gpio_pin_num;
+    }
+    else
+    {
+        // invalid pin, do nothing
+    }
+}
+
+bool isValidPin(const uint8_t pin_num)
+{
+    return LOWEST_GPIO_PIN <= pin_num && pin_num <= HIGHEST_GPIO_PIN;
 }
