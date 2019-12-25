4dd9cfb7c1c00b9bb2d5847eb1c3806e5dd4080c
JordanAceto <jordanaceto@gmail.com>
2019-12-25 16:44:33 -0500

    add sos pattern to main driver

--- RPi_OS/source/main.s
+++ RPi_OS/source/main.s
@@ -1,7 +1,8 @@
 @ simple sign of life program for Raspberry Pi 3b+
-@ blinks a LED on GPIO17 using PSP_GPIO and PSP_TIME modules
-@ or turns a led on and off with a switch on GPIO21
-@ depending on what I'm working on at the moment
+@ uses PSP_GPIO and PSP_TIME modules to blink a
+@ LED on GPIO17 in an sos pattern when a switch
+@ on GPIO21 is pulled high
+
 
 
 /***************************************************************************************************
@@ -18,7 +19,7 @@
 .equ        SWITCH_PIN,         21
 .equ        LED_PIN,            17
 
-.equ        DELAY_TIME_uSec,    1000000
+.equ        DELAY_TIME_mSec,    100
 
 
 /***************************************************************************************************
@@ -47,8 +48,12 @@ mov     r0,     #LED_PIN
 mov     r1,     #GPIO_OUTPUT
 bl      PSP_GPIO_Set_Pin_Mode   @ set gpio 17 to output
 
-mov     r4,     #GPIO_HIGH      @ led signal, this will toggle to set led on/off (for the blinky part)
+sos     .req    r4              @ sos pattern
+ldr     sos,    =sos_pattern    @ load the address
+ldr     sos,    [sos]           @ then load the actual contents
 
+index   .req    r5              @ index into the sos pattern
+mov     index,  #0              @ start at index 0
 
 
 /***************************************************************************************************
@@ -60,28 +65,26 @@ mov     r4,     #GPIO_HIGH      @ led signal, this will toggle to set led on/off
 loop$:                                  @ main loop label
 
 mov     r0,     #SWITCH_PIN
-bl      PSP_GPIO_Read_Pin               @ read pin 21
+bl      PSP_GPIO_Read_Pin               @ read the switch
 
 mov     r1,     r0                      @ store the result in r1
 
-mov     r0,     #LED_PIN
-bl      PSP_GPIO_Write_Pin              @ write the value to pin 17
+mov     r0,     #LED_PIN                @ get set up to write to the LED
+lsl     r1,     index                   @ shift the result of the switch read by the index
+and     r1,     sos                     @ and check if the sos pattern is high at that shifted index
+bl      PSP_GPIO_Write_Pin              @ LED turns on if the switch is high and the pattern at the index is high
 
-@ button above
-@+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-@ blinky below
+add     index,  #1                      @ increment the index
+and     index,  #31                     @ and wrap around when the index goes out of bounds
 
-@ eor     r4,     r4,     #1              @ toggle the led signal by xor'ing it with 1
-
-@ mov     r0,     #LED_PIN
-@ mov     r1,     r4                      @ the led signal, 0 or 1
-@ bl      PSP_GPIO_Write_Pin              @ write to the led
-
-@ ldr     r0,     =#DELAY_TIME_uSec
-@ bl      PSP_Time_Delay_Microseconds
+ldr     r0,     =#DELAY_TIME_mSec       @ kill time
+bl      PSP_Time_Delay_Milliseconds
 
 b       loop$                           @ go back to the top of the main loop
 
 
 .section    .data
 .align      2
+
+sos_pattern:
+.int    0b00000000010101011101110111010101  @ long pause, ...---...
