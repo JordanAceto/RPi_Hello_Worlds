10bde1ad5b830d7c7ecfbd9081ed36c2593c4767
JordanAceto <jordanaceto@gmail.com>
2019-12-25 12:48:02 -0500

    clean up main, add constants

--- RPi_OS/source/main.s
+++ RPi_OS/source/main.s
@@ -3,16 +3,34 @@
 @ or turns a led on and off with a switch on GPIO21
 @ depending on what I'm working on at the moment
 
-.section    .init
-.globl      _start
 
-_start:
+/***************************************************************************************************
+ *
+ *                                     Constant Definitions
+ *
+ ***************************************************************************************************/
+.equ        GPIO_INPUT,         0
+.equ        GPIO_OUTPUT,        1
+
+.equ        GPIO_LOW,           0
+.equ        GPIO_HIGH,          1
+
+.equ        SWITCH_PIN,         21
+.equ        LED_PIN,            17
+
+.equ        DELAY_TIME_uSec,    1000000
+
 
 /***************************************************************************************************
  *
  *                                           Setup
  *
  ***************************************************************************************************/
+ 
+.section    .init
+.globl      _start
+
+_start:
  
 b main
 
@@ -21,13 +39,16 @@ b main
 main:
 mov     sp,     #0x8000         @ set stack pointer to initial value
 
-mov     r0,     #21             @ pin num, gpio 21
-mov     r1,     #0              @ pin mode, 0 for input
+mov     r0,     #SWITCH_PIN
+mov     r1,     #GPIO_INPUT
 bl      PSP_GPIO_Set_Pin_Mode   @ set gpio 21 to input
 
-mov     r0,     #17             @ pin num, gpio 17
-mov     r1,     #1              @ pin mode, 1 for output
+mov     r0,     #LED_PIN
+mov     r1,     #GPIO_OUTPUT
 bl      PSP_GPIO_Set_Pin_Mode   @ set gpio 17 to output
+
+mov     r4,     #GPIO_HIGH      @ led signal, this will toggle to set led on/off (for the blinky part)
+
 
 
 /***************************************************************************************************
@@ -38,13 +59,26 @@ bl      PSP_GPIO_Set_Pin_Mode   @ set gpio 17 to output
 
 loop$:                                  @ main loop label
 
-mov     r0,     #21
+mov     r0,     #SWITCH_PIN
 bl      PSP_GPIO_Read_Pin               @ read pin 21
 
 mov     r1,     r0                      @ store the result in r1
 
-mov     r0,     #17
+mov     r0,     #LED_PIN
 bl      PSP_GPIO_Write_Pin              @ write the value to pin 17
+
+@ button above
+@+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+@ blinky below
+
+@ eor     r4,     r4,     #1              @ toggle the led signal by xor'ing it with 1
+
+@ mov     r0,     #LED_PIN
+@ mov     r1,     r4                      @ the led signal, 0 or 1
+@ bl      PSP_GPIO_Write_Pin              @ write to the led
+
+@ ldr     r0,     =#DELAY_TIME_uSec
+@ bl      PSP_Time_Delay_Microseconds
 
 b       loop$                           @ go back to the top of the main loop
 
